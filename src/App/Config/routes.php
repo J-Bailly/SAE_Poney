305ded7a8b9382d0b0596a29c6fdc3a160cf2b98
Julien <julien.bailly2@etu.univ-orleans.fr>
2025-01-17 19:52:43 +0100

    Revert "nouvelle arborescence"
    This reverts commit 0ee376cf793e7f68a49b8d53f5eb20b306f75d87.

--- src/App/Config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    '/' => [\App\Controllers\HomeController::class, 'index'],
-    '/login' => [\App\Controllers\AuthController::class, 'login'],
-    '/signup' => [\App\Controllers\AuthController::class, 'signup'],
-    '/planning' => [\App\Controllers\PlanningController::class, 'show'],
-];
-?>
